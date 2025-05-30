4674139186348caf45a43ddef0bd54e1ffce6c86
Starling Germosen <sgermosen@outlook.com>
2025-05-29 20:57:38 -0400

    Adding our first api mplementation

---
+++ DenounceBeasts.API/Entities/District.cs
@@ -0,0 +1,19 @@
+﻿namespace DenounceBeasts.API.Entities
+{
+    public class District
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int MunicipalityId { get; set; }
+
+        // Relaciones Uno a Muchos (extremo muchos)
+        public virtual Municipality Municipality { get; set; }
+
+        // Relaciones Uno a Muchos (extremo uno)
+        //public virtual ICollection<Complaint> Complaints { get; set; } = new List<Complaint>();
+    }
+}
